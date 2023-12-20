d8cc22f6db29973438ee1f986d81eb9084114dfe
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2023-12-20 19:56:00 +0700

    add lesson complete layout

---
+++ client/app/src/main/java/com/example/a4lingo/CompleteLessonActivity.java
@@ -0,0 +1,14 @@
+package com.example.a4lingo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompleteLessonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_lesson);
+    }
+}
