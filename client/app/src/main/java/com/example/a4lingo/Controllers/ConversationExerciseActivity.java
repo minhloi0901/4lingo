e939acacaa16c7cb2fb57e8e88559b8d782de510
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2023-12-23 23:35:36 +0700

    not finished conversation activity

--- client/app/src/main/java/com/example/a4lingo/Controllers/ConversationExerciseActivity.java
+++ client/app/src/main/java/com/example/a4lingo/Controllers/ConversationExerciseActivity.java
@@ -1,25 +1,36 @@
 package com.example.a4lingo.Controllers;
 
 import android.content.Intent;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.LeadingMarginSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
 import com.example.a4lingo.R;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ConversationExerciseActivity extends MainActivity{
+    private String[][] messages = new String[][] {
+            {"Hi! What can I do for you?"},
+            {"Hi professor. I wanted to talk to you about my grade for the last assignment.", "Well I got  lower grade than I was expecting and was hoping to better understand what I did wrong."},
+            {"Hi! What can I do for you?"},
+            {"Choice A", "Choice B", "Choice C", "Choice D"}
+    };
+    private int currentMessageIndex = 0;
+    private LinearLayout root = null;
+    private View v = null;
     @Override
     protected void renderLayout() {
         super.renderLayout();
-        LinearLayout root = (LinearLayout) findViewById(R.id.content);
+        root = (LinearLayout) findViewById(R.id.content);
         LayoutInflater layoutInflater = LayoutInflater.from(this);
-        View v = layoutInflater.inflate(R.layout.activity_conversation_exercise, root, false);
+        v = layoutInflater.inflate(R.layout.activity_conversation_exercise, root, false);
 
         renderAnInstance(v);
 
@@ -39,32 +50,75 @@ public class ConversationExerciseActivity extends MainActivity{
                 startActivity(intent);
             }
         });
-
-        TextView checkResBtn = findViewById(R.id.checkResButton);
-        checkResBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 
     private void renderAnInstance(View v) {
         LinearLayout linearLayout = v.findViewById(R.id.messagesLayout);
 
-        String text = "Hi! What can I do for you?";
-        addNpcMessage(linearLayout, text);
-
-        int npcMessageIndex = 1;
-        renderResponsesForSelecting(linearLayout, npcMessageIndex);
-
-        text = "Hi Professor. I wanted to talk to you about my grade for the last assignment.";
-        addUserMessage(linearLayout, text);
+        addNpcMessage(linearLayout);
     }
 
-    private void renderResponsesForSelecting(LinearLayout linearLayout, int npcMessageIndex) {
+    private void renderResponsesForSelecting(LinearLayout linearLayout) {
         LinearLayout mulChoicesLayout = (LinearLayout) getLayoutInflater().inflate(R.layout.vertical_multiple_choices_layout, linearLayout, false);
+        // Choice's layout parameters
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(40, 20, 40, 20);
 
+        // Create an instance of Random
+        Random random = new Random();
+
+        // Generate a random number within the specified range
+        int randomNumber = random.nextInt(messages[currentMessageIndex].length);
+        swap(messages[currentMessageIndex], 0, randomNumber);
+        for (int i = 0; i < messages[currentMessageIndex].length; i++){
+            boolean isCorrect = false;
+            if (i == randomNumber)
+                isCorrect = true;
+
+            mulChoicesLayout.addView(getAChoice(linearLayout, mulChoicesLayout, layoutParams, messages[currentMessageIndex][i], isCorrect));
+        }
+
+        linearLayout.addView(mulChoicesLayout);
+    }
+
+    private static <T> void swap(T[] array, int index1, int index2) {
+        if (index1 >= 0 && index1 < array.length && index2 >= 0 && index2 < array.length) {
+            T temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        } else {
+            // Handle invalid indices if needed
+            System.out.println("Invalid indices");
+        }
+    }
+
+    @NonNull
+    private TextView getAChoice(LinearLayout linearLayout, LinearLayout mulChoicesLayout, LinearLayout.LayoutParams layoutParams, String text, boolean isCorrectAnswer) {
+        TextView choice = new TextView(mulChoicesLayout.getContext());
+        choice.setLayoutParams(layoutParams);
+        choice.setBackgroundResource(R.drawable.border_gray);
+        choice.setText(text);
+        choice.setTextSize(26);
+        choice.setTag(isCorrectAnswer);
+        choice.setPadding(40, 20, 40, 20);
+        choice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isCorrect = (boolean) choice.getTag();
+                if (isCorrect){
+                    linearLayout.removeView(mulChoicesLayout);
+                    addUserMessage(linearLayout, text);
+                }
+                else{
+                    choice.setBackgroundResource(R.drawable.border_green);
+                }
+            }
+        });
+
+        return choice;
     }
 
     private void addUserMessage(LinearLayout linearLayout, String text) {
@@ -79,13 +133,25 @@ public class ConversationExerciseActivity extends MainActivity{
 
         TextView userMessageLayout = userLayout.findViewById(R.id.messageTextView);
         userMessageLayout.setText(indentText(text));
-
         linearLayout.addView(userLayout);
+        currentMessageIndex++;
+
+
+        TextView continueBtn = v.findViewById(R.id.checkResButton);
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkCompletion();
+                addNpcMessage(linearLayout);
+            }
+        });
+
     }
 
-    private void addNpcMessage(LinearLayout linearLayout, String text) {
+    private void addNpcMessage(LinearLayout linearLayout) {
         LinearLayout npcLayout = (LinearLayout) getLayoutInflater().inflate(R.layout.conversation_npc_item, linearLayout, false);
 
+        // Set audio
         ImageView audioBtn = npcLayout.findViewById(R.id.audioButton);
         audioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,10 +159,36 @@ public class ConversationExerciseActivity extends MainActivity{
                 // start Audio
             }
         });
-        TextView npcMessageLayout = npcLayout.findViewById(R.id.messageTextView);
-        npcMessageLayout.setText(indentText(text));
 
+        // Set message
+        TextView npcMessageLayout = npcLayout.findViewById(R.id.messageTextView);
+        npcMessageLayout.setText(indentText(messages[currentMessageIndex++][0]));
+
+        // Add item
         linearLayout.addView(npcLayout);
+
+        // Set responses
+        TextView continueBtn = v.findViewById(R.id.checkResButton);
+        if (continueBtn == null){
+            Log.d("TAG", "addNpcMessage: Null");
+            return;
+        }
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkCompletion();
+                renderResponsesForSelecting(linearLayout);
+            }
+        });
+    }
+
+    private void checkCompletion() {
+        if (currentMessageIndex == messages.length){
+            Intent intent = new Intent(getApplicationContext(), CompleteLessonActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
     }
 
     private String indentText(String text) {
