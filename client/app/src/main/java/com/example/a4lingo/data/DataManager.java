174c18bc3424fd056c5de4588de5629f46791e0f
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2024-01-17 23:44:43 +0700

    Merge branch 'loi' of https://github.com/minhloi0901/4lingo into loi

--- client/app/src/main/java/com/example/a4lingo/data/DataManager.java
+++ client/app/src/main/java/com/example/a4lingo/data/DataManager.java
@@ -1,36 +1,68 @@
 package com.example.a4lingo.data;
 
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.RequestBody;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class DataManager extends AppCompatActivity {
-    final OkHttpClient client = new OkHttpClient();
-    public static final MediaType JSON = MediaType.get("application/json");
+    private String url = "http://" + "127.0.0.1" + ":" + 5000 + "/";
+    private JSONObject jsonBody; // JSON object for request body
+    private MediaType mediaType;
+    private RequestBody requestBody;
 
-    String run(String url) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
+    private RequestBody buildRequestBody(JSONObject jsonBody) {
+        this.jsonBody = jsonBody;
+        mediaType = MediaType.parse("application/json; charset=utf-8"); // Set JSON content type
+        requestBody = RequestBody.create(jsonBody.toString(), mediaType);
+        return requestBody;
     }
-    String post(String url, String json) throws IOException {
-        RequestBody body = RequestBody.create(json, JSON);
+
+    public void postRequest(JSONObject jsonData, String URL) {
+        RequestBody requestBody = buildRequestBody(jsonData);
+        OkHttpClient okHttpClient = new OkHttpClient();
         Request request = new Request.Builder()
-                .url(url)
-                .post(body)
+                .post(requestBody)
+                .url(URL)
                 .build();
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(final Call call, final IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Something went wrong:" + " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        call.cancel();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Toast.makeText(getApplicationContext(), response.body().string(), Toast.LENGTH_LONG).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
+
+
