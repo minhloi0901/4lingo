e1e62464a0eb54e06f693d921b91653723164374
quoc-trung-nguyen-hcmus <121560414+quoc-trung-nguyen-hcmus@users.noreply.github.com>
2024-01-19 14:47:05 +0700

    Update server.py

--- server/server.py
+++ server/server.py
@@ -2,14 +2,18 @@ from flask import Flask
 from routes.Users_route import user_router
 from routes.Auth_route import auth_router
 from routes.Dictionary_route import dict_router
-from routes.Achievements_route import achievement_router
+from routes.Profile_route import profile_router
+from routes.Questions_route import question_router
+from routes.Vocabularies_route import vocabulary_router
 
 app = Flask(__name__)
 
 app.register_blueprint(user_router, url_prefix='/users')
 app.register_blueprint(auth_router, url_prefix='/auth')
 app.register_blueprint(dict_router, url_prefix='/dictionary')
-app.register_blueprint(achievement_router, url_prefix='/achievements')
+app.register_blueprint(profile_router, url_prefix='/profile')
+app.register_blueprint(question_router, url_prefix='/questions')
+app.register_blueprint(vocabulary_router, url_prefix='/vocabularies')
 
 @app.route('/')
 def root():
