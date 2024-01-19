891cc1d39d67df2f509f4a0f508b89cdb6fb4b64
quoc-trung-nguyen-hcmus <121560414+quoc-trung-nguyen-hcmus@users.noreply.github.com>
2024-01-20 02:15:04 +0700

    updated api key

--- server/config/config.py
+++ server/config/config.py
@@ -6,6 +6,6 @@ config = {
     'PASSWORD': '123456',
     'DB': 'lingo',
     'SECRET_KEY': 'fdkjshfhjsdfdskfdsfdcbsjdkfdsdf',
-    'GG_API_KEY': '776026c167msh8aebcf0db7082acp192fcejsn149cde93dd69',
+    'GG_API_KEY': '49b622cb5fmsh30a92b86d0c7f52p1c0310jsn4ee0b985074a',
     'SECRET_SALT': '8'
 }
